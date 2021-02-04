6e5f0a716df01f188fb06adc7a513dd93471458d
James Tedy <james.tedy@ymail.com>
2021-02-04 21:04:37 +1100

    Modify pokemonList - Add useEffect to get all pokemons name and data - modify card to a list of pokemons card

--- src/pages/pokemonList.js
+++ src/pages/pokemonList.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getAllPokemons } from "../api/pokeApi";
 import Card from "../components/Card";
 
 const PokemonList = () => {
+  const [pokeList, setPokeList] = useState();
+
+  const initPokes = async () => {
+    let pokes = await getAllPokemons();
+    console.log(pokes);
+    setPokeList(pokes);
+  };
+
+  useEffect(() => {
+    console.log("rest");
+    initPokes();
+  }, []);
+
   return (
     <div>
       <div style={headerStyle}>
@@ -9,7 +23,15 @@ const PokemonList = () => {
         <div>Tabs div</div>
       </div>
       <div>
-        <Card />
+        <ul>
+          {pokeList
+            ? pokeList.map((data) => (
+                <li>
+                  <Card name={data.name} apiUrl={data.url} />
+                </li>
+              ))
+            : null}
+        </ul>
       </div>
     </div>
   );
