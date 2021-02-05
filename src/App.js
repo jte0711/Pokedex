c237d12c1d42c2b27c3e0aa6884a085431702df2
James Tedy <james.tedy@ymail.com>
2021-02-05 17:43:31 +1100

    Add router and header to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import "./App.css";
+import PokemonDetails from "./components/PokemonDetails";
 import PokemonList from "./pages/pokemonList";
 
 function App() {
   return (
-    <div className="App">
-      <PokemonList />
-    </div>
+    <Router>
+      <div className="App">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            paddingBottom: "50px",
+          }}
+        >
+          <Link to="/">Pokedex</Link>
+          <Link to="/pokemon/ivysaur">Pokemon details</Link>
+        </div>
+        <Switch>
+          <Route exact path="/">
+            <PokemonList />
+          </Route>
+          <Route exact path="/pokemon/:pokeId">
+            <PokemonDetails />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
