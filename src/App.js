7fd097d61adbb0994becea58124dce5bce5cbaf9
James Tedy <james.tedy@ymail.com>
2021-02-05 20:24:38 +1100

    Add ReactContext to the file

--- src/App.js
+++ src/App.js
@@ -1,33 +1,46 @@
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import "./App.css";
-import PokemonDetails from "./components/PokemonDetails";
+import PokemonDetails from "./pages/PokemonDetails";
 import PokemonList from "./pages/pokemonList";
+import PokeContext from "./config/pokeContext";
+import { useState } from "react";
 
 function App() {
+  const [myPokemon, setMyPokemon] = useState([]);
+
   return (
-    <Router>
-      <div className="App">
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            paddingBottom: "50px",
-          }}
-        >
-          <Link to="/">Pokedex</Link>
-          <Link to="/pokemon/ivysaur">Pokemon details</Link>
+    <PokeContext
+      value={{
+        myPokemon: myPokemon,
+        setMyPokemon: (newData) => {
+          setMyPokemon(newData);
+        },
+      }}
+    >
+      <Router>
+        <div className="App">
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              paddingBottom: "50px",
+            }}
+          >
+            <Link to="/">Pokedex</Link>
+            <Link to="/pokemon/ivysaur">Pokemon details</Link>
+          </div>
+          <Switch>
+            <Route exact path="/">
+              <PokemonList />
+            </Route>
+            <Route exact path="/pokemon/:pokeId">
+              <PokemonDetails />
+            </Route>
+          </Switch>
         </div>
-        <Switch>
-          <Route exact path="/">
-            <PokemonList />
-          </Route>
-          <Route exact path="/pokemon/:pokeId">
-            <PokemonDetails />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+      </Router>
+    </PokeContext>
   );
 }
 
