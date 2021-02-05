84b365f714a986898e09820ae0dd59eb194fcea3
James Tedy <james.tedy@ymail.com>
2021-02-05 18:17:19 +1100

    Add WideCard for MyPokemonList

---
+++ src/components/WideCard.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const WideCard = (props) => {
+  const [imgUrl, setImgUrl] = useState("");
+  const [type, setType] = useState([]);
+
+  const initPoke = async () => {
+    const pokeDeets = await getPokemon(props.name);
+    //console.log(pokeDeets);
+    setImgUrl(pokeDeets.sprites.front_default); //url
+    setType(pokeDeets.types); //list, check api call
+  };
+
+  useEffect(() => {
+    initPoke();
+  }, [props]);
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        width: "40%",
+        border: "solid",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRight: "solid",
+          width: "25%",
+        }}
+      >
+        <img src={imgUrl} />
+      </div>
+      <div style={{ display: "flex", flexDirection: "column", width: "75%" }}>
+        <p>Nickname</p>
+        <ul>
+          <li>Type 1</li>
+          <li>Type 2</li>
+        </ul>
+        <p>More Details</p>
+      </div>
+    </div>
+  );
+};
+
+export default WideCard;
