3c9c2618bf16c4b186928c3faa72dce2f5b5109f
James Tedy <james.tedy@ymail.com>
2021-02-06 17:14:07 +1100

    Modify context, state, etc
    - Add mock data to the app
    - Add a new context data
    - Add new link and route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import "./App.css";
 import PokemonDetails from "./pages/PokemonDetails";
-import PokemonList from "./pages/pokemonList";
+import PokemonList from "./pages/PokemonList";
+import MyPokemonList from "./pages/MyPokemonList";
 import PokeContext from "./config/pokeContext";
 import { useState } from "react";
 
 function App() {
-  const [myPokemon, setMyPokemon] = useState([]);
+  const [myPokemon, setMyPokemon] = useState([
+    {
+      name: "squirtle",
+      nickname: "turtle",
+      url: "https://pokeapi.co/api/v2/pokemon/7/",
+    },
+    {
+      name: "bulbasaur",
+      nickname: "moving plant",
+      url: "https://pokeapi.co/api/v2/pokemon/1/",
+    },
+  ]);
+  const [ownedPokemon, setOwnedPokemon] = useState([
+    { name: "squirtle", owned: 1 },
+  ]);
 
   return (
-    <PokeContext
+    <PokeContext.Provider
       value={{
         myPokemon: myPokemon,
         setMyPokemon: (newData) => {
           setMyPokemon(newData);
+        },
+        ownedPokemon: ownedPokemon,
+        setOwnedPokemon: (newData) => {
+          setOwnedPokemon(newData);
         },
       }}
     >
@@ -28,7 +47,7 @@ function App() {
             }}
           >
             <Link to="/">Pokedex</Link>
-            <Link to="/pokemon/ivysaur">Pokemon details</Link>
+            <Link to="/mypokemon">My Pokemons</Link>
           </div>
           <Switch>
             <Route exact path="/">
@@ -37,10 +56,13 @@ function App() {
             <Route exact path="/pokemon/:pokeId">
               <PokemonDetails />
             </Route>
+            <Route exact path="/mypokemon">
+              <MyPokemonList />
+            </Route>
           </Switch>
         </div>
       </Router>
-    </PokeContext>
+    </PokeContext.Provider>
   );
 }
 
