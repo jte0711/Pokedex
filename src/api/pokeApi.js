61a8424d2703d18e93f470bf6a82d25eba1a3d7e
James Tedy <james.tedy@ymail.com>
2021-02-07 12:27:35 +1100

    Add async await to the functions

--- src/api/pokeApi.js
+++ src/api/pokeApi.js
@@ -2,7 +2,7 @@ import { apiUrl, pokes } from "../config/env";
 
 const axios = require("axios");
 
-function getAllPokemons() {
+async function getAllPokemons() {
   //check if pokemons stored in localStorage
   const localRes = localStorage.getItem(pokes);
   if (localRes != null) {
@@ -11,38 +11,56 @@ function getAllPokemons() {
 
   //request pokemons data
   const endpoint = apiUrl + "pokemon/";
-  axios
-    .get(endpoint)
-    .then((res) => {
-      let result = res.data.results;
-      result = result.map((data) => {
-        return Object.assign(data, { owned: 0 });
-      });
-      localStorage.setItem(pokes, JSON.stringify(result));
-      return result; //result only up to twenty, adjust limiter or make a code to iterate through the rest
-    })
-    .catch((err) => {
-      console.log(err);
+  try {
+    const res = await axios.get(endpoint);
+    let result = res.data.results;
+    result = result.map((data) => {
+      return Object.assign(data, { owned: 0 });
     });
+    localStorage.setItem(pokes, JSON.stringify(result));
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+
+  // axios
+  //   .get(endpoint)
+  //   .then((res) => {
+  //     let result = res.data.results;
+  //     result = result.map((data) => {
+  //       return Object.assign(data, { owned: 0 });
+  //     });
+  //     localStorage.setItem(pokes, JSON.stringify(result));
+  //     return result; //result only up to twenty, adjust limiter or make a code to iterate through the rest
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
 }
 
-function getPokemon(name) {
+async function getPokemon(name) {
   const localRes = localStorage.getItem(name);
   if (localRes != null) {
     return JSON.parse(localRes);
   }
 
   const endpoint = apiUrl + "pokemon/" + name;
-
-  axios
-    .get(endpoint)
-    .then((res) => {
-      localStorage.setItem(name, JSON.stringify(res.data));
-      return res.data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  try {
+    const res = await axios.get(endpoint);
+    localStorage.setItem(name, JSON.stringify(res.data));
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+  // axios
+  //   .get(endpoint)
+  //   .then((res) => {
+  //     localStorage.setItem(name, JSON.stringify(res.data));
+  //     return res.data;
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
 }
 
 export { getAllPokemons, getPokemon };
